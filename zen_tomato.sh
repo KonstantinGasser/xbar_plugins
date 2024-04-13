c6232aa3655a15fb2f725787e43c1c37aaafde6e
KonstantinGasser <konstantin.gasser@me.com>
2024-04-13 22:50:43 +0200

    feat: break focus time and continue fresh after a break

--- zen_tomato.sh
+++ zen_tomato.sh
@@ -17,7 +17,7 @@
 #  - make ZEN_RANGE soft limit with a visual reminder to take a break :)
 
 STEP_CHAR="⬤"
-STEP_CHARS=("➊" "➋" "➌" "◰") # index 3 not needed since range of quarters is [0,4)
+BREAK_PREFIX="b"
 
 QUARTER_COLORS=($COLOR_ONE $COLOR_TWO $COLOR_THREE)
 
@@ -28,12 +28,63 @@ QUARTERS_IN_HOUR=4
 
 print_menu() {
 	echo "---"
-
+	
+	# show: session must not be running
 	if [ $(session_running) -eq -1 ]; then
 		echo "Let's Focus | bash='$0' | param1='start' | terminal=false | refresh=true"
-	else
-		echo "Coffee break | bash='$0' | param1='stop' | terminal=false | refresh=true"
 	fi
+
+	# show: session must be running and no current break started
+	if [ $(session_running) -eq 1 ] && [ $(break_running) -eq -1 ]; then
+		echo "Coffee break | bash='$0' | param1='break' | terminal=false | refresh=true"
+	fi
+
+	# show: session must be running and break started
+	if [ $(session_running) -eq 1 ] && [ $(break_running) -eq 1 ]; then
+		echo "Back to work | bash='$0' | param1='continue' | terminal=false | refresh=true"
+	fi
+
+	# show: session must be running. If break or not does not matter
+	if [ $(session_running) -eq 1 ]; then
+		echo "Wind down | bash='$0' | param1='stop' | terminal=false | refresh=true"
+	fi
+}
+
+add_break_prefix() {
+	local line_number=1
+
+	if [ $(wc -l < "${ZEN_FILE}") -gt 0 ]; then
+		line_number=$(wc -l < "${ZEN_FILE}")
+	fi
+
+	sed -i '' "${line_number} s/^/${BREAK_PREFIX}/" "${ZEN_FILE}"
+
+}
+
+
+remove_break_prefix() {
+	local line_number=1
+
+
+	if [ $(wc -l < "${ZEN_FILE}") -gt 0 ]; then
+		line_number=$(wc -l < "${ZEN_FILE}")
+	fi
+
+	sed -i '' "${line_number} s/^${BREAK_PREFIX}//" "${ZEN_FILE}"
+}
+
+# after a break we want to reset the timer
+update_break_continue_time() {
+	local line_number=1
+
+	if [ $(wc -l < "${ZEN_FILE}") -gt 0 ]; then
+		line_number=$(wc -l < "${ZEN_FILE}")
+	fi
+
+	local current_ts=$(tail -n1 ${ZEN_FILE} | awk -F ':' '{print $2}' | awk -F '-' '{print $1}')
+	local now_ts=$(date +%s)
+
+	sed -i '' "${line_number} s/${current_ts}/${now_ts}/" "${ZEN_FILE}"
 }
 
 passed_zen_minutes() {
@@ -52,6 +103,7 @@ passed_zen_minutes() {
 	echo $(($diff / $HOUR_IN_MINUTES))
 }
 
+# UNUSED
 # based on the provided ZEN_RANGE compute how often
 # it was reached.
 # really just a convenience function for better readabiliy.
@@ -66,6 +118,29 @@ passed_zen_minutes() {
 # => count=2
 count_times_zen_reached() {
 	echo "$(($1/$ZEN_RANGE))"
+}
+
+
+# -1: no break running
+#  1: break running
+break_running() {
+
+	# need to use -c since initally no \n is appended to the line (see case "start")
+	if [ "$(wc -c < "${ZEN_FILE}")" -le 0 ]; then 
+		echo "-1"
+		return
+	fi
+
+	# if the current session (last line in file) has the prefix b
+	# the session is paused.
+	local prefix=$(tail -n1 ${ZEN_FILE} | head -c1)
+
+	if [ "$prefix" != "$BREAK_PREFIX" ]; then
+		echo "-1"
+		return
+	fi
+
+	echo "1"
 }
 
 # -1: no session running
@@ -93,6 +168,12 @@ if [ -z "$1" ]; then
 	passed_minutes=$(passed_zen_minutes)
 
 
+	if [ $(break_running) -eq 1 ]; then
+		echo "☕️"
+		print_menu
+		exit 0
+	fi
+
 	if [ "$passed_minutes" -lt 0 ]; then
 		echo -e "\x1B[48;5;208mz\x1B[48;5;211me\x1B[48;5;213mn"
 		echo -e "\x1B[0m"
@@ -101,6 +182,9 @@ if [ -z "$1" ]; then
 		# prefix:
 		# 1) either shows number of passed hours
 		# 2) or if hours eq 0 static text
+		#
+		# divider:
+		# 1) " - "
 		#
 		# suffix:
 		# 1) shows uncompleted hour segements colors
@@ -114,6 +198,9 @@ if [ -z "$1" ]; then
 		else
 			message+="Keep going.."
 		fi
+
+		# append divider between prefix and suffix
+		message+=" - "
 		
 		# compute minutes of new started hour (range [0,60)).
 		#
@@ -131,27 +218,19 @@ if [ -z "$1" ]; then
 		# which are 2
 		remaining_quarters=$(($remaining_minutes / $QUARTER_IN_MINUTES))
 
-		# remaining quarters can be zero at which point we do not want to
-		# include a divider between the message prefix and suffix
-		if [ $remaining_quarters -gt 0 ]; then
-			message+=" "
-		fi
 
 		# for each passed quarter append a color shifted STEP_CHAR.
 		for ((i = 1; i <= $remaining_quarters; i++)); do
 			message+=" \x1B[38;5;${QUARTER_COLORS[$(($i-1))]}m${STEP_CHAR}"
-			
 		done
 
-		# NOTE: not sure what to do which this info?
-		#
 		# compute the trailing minutes last uncompleted quarter.
 		# values in range [0,14).
 		left_minutes=$(($remaining_minutes % $QUARTER_IN_MINUTES))
 	
 		# in order to not show nothing when a new hour started
 		# and a quarter has not passed yet show a dot
-		if [ $remaining_quarters -eq 0 && $left_minutes -gt 0]; then
+		if [ $remaining_quarters -eq 0 ] && [ $left_minutes -ge 0 ]; then
 			message+=" \x1B[38;5;${QUARTER_COLORS[0]}m${STEP_CHAR}"
 		fi
 
@@ -168,6 +247,52 @@ else
 				exit 0
 			fi
 			echo -n "$(date +%Y-%m-%d):$(date +%s)" >> "${ZEN_FILE}"
+			;;
+		"break")
+			if [ $(session_running) -eq -1 ]; then
+				echo "Break of session only while session is running"
+				print_menu
+				exit 0
+			fi
+			
+			if [ $(break_running) -eq 1 ]; then
+				echo "Break of session only while no break is running"
+				print_menu
+				exit 0
+			fi
+
+			# prepend BREAK_PREFIX to current session
+			if [ "$(wc -c < "${ZEN_FILE}")" -le 0 ]; then 
+				echo "Break of session only when session exists"
+				print_menu
+				exit 0
+			fi
+
+			add_break_prefix
+			;;
+		"continue")
+
+			if [ $(session_running) -eq -1 ]; then
+				echo "Continue of break only when session exists"
+				print_menu
+				exit 0
+			fi
+
+			if [ $(break_running) -eq -1 ]; then
+				echo "Continue of break only when break started"
+				print_menu
+				exit 0
+			fi
+
+
+			if [ "$(wc -c < "${ZEN_FILE}")" -le 0 ]; then 
+				echo "Continue of break only when session exists"
+				print_menu
+				exit 0
+			fi
+
+			remove_break_prefix
+			update_break_continue_time
 			;;
 		"stop") 
 			if [ $(session_running) -eq -1 ]; then
